9a68fa169e65b30f0213938863d50bf52f67c146
genus1 <roger@rogersbooks.info>
2017-12-08 00:03:10 -0800

    Added CrossPlatformInputManager

---
+++ Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+		Debug.Log("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+	}
+}
