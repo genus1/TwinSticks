1608f0fb0e4871a44ec62578952d42d7fd585245
genus1 <roger@rogersbooks.info>
2017-12-07 23:08:27 -0800

    Fixed Semicolon

--- Assets/MyScript.cs
+++ Assets/MyScript.cs
@@ -6,7 +6,7 @@ public class MyScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		print("You found bitcoin!"):
+		print("You found bitcoin!");
 	}
 	
 	// Update is called once per frame
