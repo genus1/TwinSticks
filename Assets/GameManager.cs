e53bb33906477994910652481a93505041cb21b8
genus1 <roger@rogersbooks.info>
2017-12-08 02:29:50 -0800

    Up to playback
    Missing short loop solution

---
+++ Assets/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+	public bool recording = true;
+	
+	// Update is called once per frame
+	//This will create a huge data buffer if left recording
+	void Update () {
+		if (CrossPlatformInputManager.GetButton ("Fire1")){
+			recording = false;
+		}else{
+			recording = true;
+		}
+	}
+}
---
+++ Assets/ReplaySystem.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+	private Rigidbody rigidBody;
+	private GameManager manager;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		manager = GameObject.FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (manager.recording) {
+			Record ();
+		} else {
+			PlayBack ();
+		}
+
+	}
+
+	void PlayBack () {
+		rigidBody.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		print ("Reading frame " + frame);
+		transform.position = keyFrames [frame].position;
+		transform.rotation = keyFrames [frame].rotation;
+	}
+
+	void Record () {
+		rigidBody.isKinematic = false;
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		print ("Writing Frame " + frame);
+		keyFrames [frame] = new MyKeyFrame (time, transform.position, transform.rotation);
+	}
+
+	/// <summary>
+	///  A structure for storing time, rotation and position;
+	/// </summary>
+	public struct MyKeyFrame{
+		public float frameTime;
+		public Vector3 position;
+		public Quaternion rotation;
+
+		public MyKeyFrame (float aTime, Vector3 aPosition, Quaternion aRotation){
+			frameTime = aTime;
+			position = aPosition;
+			rotation = aRotation;
+		}
+	}
+}
